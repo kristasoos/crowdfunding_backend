0039dcd8a9137c721ae7c021c3ff43c1ab5a08aa
kristasoos <99314662+kristasoos@users.noreply.github.com>
2025-08-09 12:45:02 +0800

    Wrote our first Django model and created a test fundriser

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fundraisers.apps.FundraisersConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# We inherited our class from the built-in models.Model class that comes with Django. This means that our new model has a bunch of incredibly useful functionality right out of the box. Django will even create a database table for us based on this model!
+# Our model has a bunch of attributes: title, description, etc. Each of these is itself an instance of one of the classes that comes bundled with Django. These attributes tell Django what types of fields we want in our database table
+
+class Fundraiser(models.Model):
+    # We can pass arguments to our model's field attributes to specify extra functionality.
+    # Title field cannnot be longer than 200 characters
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    goal=models.IntegerField()
+    image=models.URLField()
+    is_open=models.BooleanField()
+    # Date field should be automatically set with the current date when record is created. 
+    date_created=models.DateTimeField(auto_now_add=True)
+
+
+
+    
