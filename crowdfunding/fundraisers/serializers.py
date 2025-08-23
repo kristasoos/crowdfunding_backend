4b3fbc1663ecd785e1006cd9a3b4630b7605c079
kristasoos <99314662+kristasoos@users.noreply.github.com>
2025-08-23 10:41:56 +0800

    Worked on pledges

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -4,11 +4,13 @@ from django.apps import apps
 class FundraiserSerializer(serializers.ModelSerializer):
     # we set the owner to be read only due to not being able to create project on behalf of other people
     owner=serializers.ReadOnlyField(source='owner.id')
+   
     class Meta:
         model = apps.get_model('fundraisers.Fundraiser')
         fields = '__all__'
 
 class PledgeSerializer(serializers.ModelSerializer):
+    supporter=serializers.ReadOnlyField(source='supporter.id')
     class Meta:
         model=apps.get_model('fundraisers.PLedge')
         fields='__all__'
