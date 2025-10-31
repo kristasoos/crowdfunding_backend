118b7cb0d41182054691545c348ce30cf7f4f7a2
kristasoos <99314662+kristasoos@users.noreply.github.com>
2025-10-31 16:35:04 +0800

    Changed the users serializer file

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -13,4 +13,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
         }
 
         def create(self, validated_data):
-            return CustomUser.objects.create_user(**validated_data)
+            user = CustomUser(
+                email=validated_data['email'],
+                username=validated_data['username']
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
+
+
+
+    
