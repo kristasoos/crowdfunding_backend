ce92be7366fa2996c82d3abef73b2a744ebc4f0a
kristasoos <99314662+kristasoos@users.noreply.github.com>
2025-11-01 16:31:27 +0800

    Changes to my user behaviour

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -3,14 +3,14 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['username', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        user = CustomUser(**validated_data)
+        user = CustomUser( username=validated_data['username'], email=validated_data.get('email', ''))
         if password:
             user.set_password(password)  # This hashes the password
         user.save()
